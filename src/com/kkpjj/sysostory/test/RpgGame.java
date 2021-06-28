4fe1f91797d22ce34bb37e8f80cb647e8980be17
rpaxk <rpaxk@210.90.51.89>
2021-06-28 18:51:27 +0900

    권성령

--- src/com/kkpjj/sysostory/test/RpgGame.java
+++ src/com/kkpjj/sysostory/test/RpgGame.java
@@ -6,7 +6,10 @@ import java.awt.event.*;
 
 public class RpgGame {
 	public static void main(String[] ar) {
-		RpgGame_frame rg = new RpgGame_frame();
+		new RpgGame_frame();
+		
+		
+	
 	}
 }
 
@@ -17,7 +20,14 @@ class RpgGame_frame extends JFrame implements Runnable, KeyListener {
 	boolean keyRight = false;
 	boolean playerMove = false;
 	Toolkit tk = Toolkit.getDefaultToolkit();
-	Image background = new ImageIcon("images/test.png").getImage();
+	
+	
+	
+	
+	Image background = new ImageIcon("images/test.png").getImage().getScaledInstance(800, 600, 0);
+	
+	
+	
 	Image img = new ImageIcon("images/2jo.PNG").getImage(); // 이미지를 불러옵니다.
 	Image buffimg;// 더블버퍼링용 입니다.
 	Graphics gc;
@@ -27,24 +37,43 @@ class RpgGame_frame extends JFrame implements Runnable, KeyListener {
 	int moveStatus; // 케릭터가 어디를 바라보는지 방향을 받을 변수
 
 	RpgGame_frame() {
-		setTitle("테스트");
-		setSize(800, 600);
+		
+		JFrame mf = new JFrame();
+		
+		JPanel mainPanel = new JPanel();
+		
+		mf.setBounds(300, 75, 810, 630);
+		mf.setLayout(null);
+		
+		
+		mainPanel.setBounds(0, 0, 800, 420);
+		
+		Image background = new ImageIcon("images/map.png").getImage().getScaledInstance(800, 600, 0);
+		JLabel label = new JLabel(new ImageIcon(background));
+		
+		mainPanel.add(label);
+		
+		mf.add(mainPanel);
+		
 		init();
 		start();
-
+		
+		
+		
 		Dimension screen = tk.getScreenSize();
 		int xpos = (int) (screen.getWidth() / 2 - getWidth() / 2);
 		int ypos = (int) (screen.getHeight() / 2 - getHeight() / 2);
-		setLocation(xpos, ypos);
-		setResizable(false);
-		setVisible(true);
+		mf.setLocation(xpos, ypos);
+		mf.setResizable(false);
+		mf.setVisible(true);
+		mf.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public void init() {
 		x = 100;
 		y = 100;
 		moveStatus = 3;
-		//케릭터가 시작할때 바라보는 방향은 아래쪽입니다.
+		// 케릭터가 시작할때 바라보는 방향은 아래쪽입니다.
 		// 0 : 위쪽, 1 : 오른쪽, 2 : 왼쪽, 3 : 아래쪽
 	}
 
@@ -74,29 +103,29 @@ class RpgGame_frame extends JFrame implements Runnable, KeyListener {
 	}
 
 	public void update(Graphics g) {
-		//더블 버퍼링을 이용해 버퍼에 그려진것을 가져옵니다.
+		// 더블 버퍼링을 이용해 버퍼에 그려진것을 가져옵니다.
 		DrawImg();
 		g.drawImage(buffimg, 0, 0, this);
 	}
 
 	public void DrawImg() {
-		//		gc.setFont(new Font("Default", Font.BOLD, 20));
-		//		gc.drawString(Integer.toString(cnt), 50, 50);
-		//		gc.drawString(Integer.toString((playerMove) ? 1 : 0), 200, 50);
-		//위는 단순히 무한루프 적용여부와 케릭터 방향 체크를 위해
-		//눈으로 보면서 테스트할 용도로 쓰이는 텍스트 표출입니다.
+		// gc.setFont(new Font("Default", Font.BOLD, 20));
+		// gc.drawString(Integer.toString(cnt), 50, 50);
+		// gc.drawString(Integer.toString((playerMove) ? 1 : 0), 200, 50);
+		// 위는 단순히 무한루프 적용여부와 케릭터 방향 체크를 위해
+		// 눈으로 보면서 테스트할 용도로 쓰이는 텍스트 표출입니다.
 
 		MoveImage(img, x, y, 90, 78);
-		//케릭터를 걸어가게 만들기 위해 추가로 만든 메소드 입니다.
+		// 케릭터를 걸어가게 만들기 위해 추가로 만든 메소드 입니다.
 	}
 
 	public void MoveImage(Image img, int x, int y, int width, int height) {
-		//케릭터 이미지, 케릭터 위치, 케릭터 크기를 받습니다.
-		//받은 값을 이용해서 위의 이미지칩셋에서 케릭터를 잘라내
-		//표출하도록 계산하는 메소드 입니다.
+		// 케릭터 이미지, 케릭터 위치, 케릭터 크기를 받습니다.
+		// 받은 값을 이용해서 위의 이미지칩셋에서 케릭터를 잘라내
+		// 표출하도록 계산하는 메소드 입니다.
 
 		gc.setClip(x, y, width, height);
-		//현재 좌표에서 케릭터의 크기 만큼 이미지를 잘라 그립니다.
+		// 현재 좌표에서 케릭터의 크기 만큼 이미지를 잘라 그립니다.
 
 		if (playerMove) { // 케릭터의 움직임 여부를 판단합니다.
 			if (cnt / 10 % 4 == 0) {
@@ -111,20 +140,20 @@ class RpgGame_frame extends JFrame implements Runnable, KeyListener {
 			} else if (cnt / 10 % 4 == 3) {
 				gc.drawImage(img, x - (width * 1), y - (height * moveStatus), this);
 			}
-			//케릭터의 방향에 따라 걸어가는 모션을 취하는 
-			//케릭터 이미지를 시간차를 이용해 순차적으로 그립니다.
+			// 케릭터의 방향에 따라 걸어가는 모션을 취하는
+			// 케릭터 이미지를 시간차를 이용해 순차적으로 그립니다.
 
 		} else {
 			gc.drawImage(img, x - (width * 1), y - (height * moveStatus), this);
-			//케릭터가 움직이지 않으면 정지한 케릭터를 그립니다.
+			// 케릭터가 움직이지 않으면 정지한 케릭터를 그립니다.
 
 		}
 	}
 
 	// 0 : 위쪽, 1 : 오른쪽, 2 : 아래쪽, 3 : 왼쪽
 	public void keyProcess() {
-		//여기서는 단순 케릭터가 이동하는 좌표 말고도
-		//케릭터의 움직임 여부및 방향을 체크 합니다.
+		// 여기서는 단순 케릭터가 이동하는 좌표 말고도
+		// 케릭터의 움직임 여부및 방향을 체크 합니다.
 		playerMove = false;
 
 		if (keyUp) {
