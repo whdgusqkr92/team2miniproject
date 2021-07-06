59998f0e2aa350eafc57f050527efeb40dc3886a
rpaxk <rpaxk@210.90.51.89>
2021-07-06 09:53:03 +0900

    권성령!

--- src/com/kkpjj/sysostory/view/MainFrame.java
+++ src/com/kkpjj/sysostory/view/MainFrame.java
@@ -2,6 +2,11 @@ package com.kkpjj.sysostory.view;
 
 import javax.swing.JFrame;
 
+import com.kkpjj.sysostory.view.character.FieldCharacterBattle;
+import com.kkpjj.sysostory.view.character.VillageView;
+import com.kkpjj.sysostory.view.skill.Skill;
+import com.kkpjj.sysostory.view.skill.SkillList;
+
 public class MainFrame extends JFrame {
 
 	private JFrame mf;
@@ -14,8 +19,13 @@ public class MainFrame extends JFrame {
 		mf.setSize(800, 600);
 		mf.setLayout(null);
 		
-    new StartScreen();
-    
+//    new StartScreen();
+//    new FieldCharacterBattle(mf);
+//		new VillageView(mf);
+//		new Skill();
+//		new SkillList(mf);
+		new FieldCharacterBattle(mf);
+		
 		mf.setLocationRelativeTo(null);
 		mf.setResizable(true);
 		mf.setVisible(true);
